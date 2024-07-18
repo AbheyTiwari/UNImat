08de5cb77298743371ac2d366a7de68dd5c674c7
univaultplus@gmail.com <univaultplus@gmail.com>
2024-07-18 11:56:59 +0000

    Created couse_detatails; Edited Base, Library, Saved and Home

--- client_code/Library/__init__.py
+++ client_code/Library/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import LibraryTemplate
 from anvil import *
+import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.tables as tables
@@ -12,5 +13,11 @@ class Library(LibraryTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.load_course()
+    
 
     # Any code you write here will run before the form opens.
+  def load_course(self):
+    courses = anvil.server.call("get_course_detail").search()
+    for course in courses:
+      print(course["Name"])
--- client_code/Saved/__init__.py
+++ client_code/Saved/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import SavedTemplate
 from anvil import *
+import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.tables as tables
@@ -12,5 +13,3 @@ class Saved(SavedTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
